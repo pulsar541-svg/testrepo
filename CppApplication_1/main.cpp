5bc2f8360e347e3d0324c9bfc1a95bff60b223e7
pulsar541 <e.useinov@gmail.com>
2013-12-19 16:58:13 +0400

    sdefsgesrgedrge

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Евгений
+ *
+ * Created on 19 Декабрь 2013 г., 16:57
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 555555555555555555555555555555555555
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
